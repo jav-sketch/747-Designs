80ba61dd9b17b98f512520c24877ccd96526e83c
[jav-sketch] <[javonbrowne@gmail.com]>
2020-08-13 13:54:00 -0400

    Commit

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Design_Site.apps.Design_SiteConfig',
+    'Design_Site.apps.DesignSiteConfig',
 ]
 
 MIDDLEWARE = [
