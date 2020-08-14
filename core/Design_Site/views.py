6a930893fb57706bb82255beefad59930d8e346b
[jav-sketch] <[javonbrowne@gmail.com]>
2020-08-14 11:02:01 -0400

    Commit

--- core/Design_Site/views.py
+++ core/Design_Site/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
+from .models import *
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Welcome to 747 Designs & Prints!")
+# def index(request):
+#     return render(request, "index.html")
+    
+
+class HomeView(View):
+    model = Customer
+    template_name = "index.html"
