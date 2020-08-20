b3fcfe946feaab254636c47c50ea9448985ce071
[jav-sketch] <[javonbrowne@gmail.com]>
2020-08-20 13:52:44 -0400

    Update Admin.py

--- core/Design_Site/admin.py
+++ core/Design_Site/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from Design_Site.models import Customer
+from Design_Site.models import *
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(Description)
+admin.site.register(Product)
