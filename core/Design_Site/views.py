294c6e157e7682d18fa4a17d6e8a8d8069c34822
[jav-sketch] <[javonbrowne@gmail.com]>
2020-08-20 15:29:44 -0400

    Update Intro to Index

--- core/Design_Site/views.py
+++ core/Design_Site/views.py
@@ -11,4 +11,12 @@ from .models import *
 
 
 class HomeView(TemplateView):
+
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['intros'] = Intro.objects.all()[:5]
+        return context
+
+    
