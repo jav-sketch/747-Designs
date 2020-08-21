e07fd35d7c826033889c9d6192d5846679b43489
[jav-sketch] <[javonbrowne@gmail.com]>
2020-08-21 16:53:59 -0400

    Updated Views.py

--- core/Design_Site/views.py
+++ core/Design_Site/views.py
@@ -11,11 +11,10 @@ from .models import *
 
 
 class HomeView(TemplateView):
-
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['intros'] = Intro.objects.all()[:1]
+        context['intros'] = Intro.objects.all()[:3]
         return context
 
