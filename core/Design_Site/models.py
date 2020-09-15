94b91be0988d1c749a4a56d3796319549878ea88
[jav-sketch] <[javonbrowne@gmail.com]>
2020-09-15 11:26:37 -0400

    Updated Intro Model

--- core/Design_Site/models.py
+++ core/Design_Site/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Intro(models.Model):
     title = models.CharField(max_length=50, null=True)
-    description = models.CharField(max_length=500)
+    description = models.TextField(max_length=500)
 
     def __str__(self):
         return self.title
